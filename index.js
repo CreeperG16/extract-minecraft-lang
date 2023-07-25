535bc8559aa25d1235b0f2b5e1139ce09107eb32
Tamas Papp <86970314+CreeperG16@users.noreply.github.com>
2023-07-25 13:16:37 +0000

    Add all option, extract all current versions langguages

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const { getLang } = require("./language.js");
 
-const [,, version, outDir] = process.argv;
+async function getAllVersions() {
+  const { versions } = require("minecraft-data");
 
-(async () => {
+  for (const { minecraftVersion: version } of versions.bedrock) {
+    try {
+      console.log(`> Downloading lang file for ${version}...`);
+      await getLang("./language", version);
+      console.log(`> Downloaded and parsed language file for version ${version}.`);
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+
+  process.exit(0);
+}
+
+async function getOneVersion() {
+  const [,, version, outDir] = process.argv;
+
   if (version === undefined || outDir === undefined) throw new Error("Usage: node index.js <version> <outDir>");
 
   await getLang(outDir, version);
 
   console.log(`Downloaded and parsed language file for version ${version}.`);
-})()
+
+  process.exit(0);
+}
+
+if (process.argv.includes("--all")) {
+  getAllVersions();
+} else {
+  getOneVersion();
+}
